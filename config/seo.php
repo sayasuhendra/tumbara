1f0a475a24e6c1ec3d55bac43249ed36f541c3b5
Suhendra <sayasuhendra@gmail.com>
2025-06-03 08:37:36 +0700

    fixed seo.

--- config/seo.php
+++ config/seo.php
@@ -75,14 +75,14 @@ return [
          * Use this setting to provide a suffix that will be added after the title on each page.
          * If you don't want a suffix, you should specify an empty string.
          */
-        'suffix' => 'PT Tumbara',
+        'suffix' => ' - PT Tumbara',
 
         /**
          * Use this setting to provide a custom title for the homepage. We will not use the suffix on the homepage,
          * so you'll need to add the suffix manually if you want that. If set to null, we'll determine the title
          * just like the other pages.
          */
-        'homepage_title' => 'PT Tumbara',
+        'homepage_title' => 'PT Tumbuh Nusantara Utama (Tumbara)',
     ],
 
     'description' => [
